39f37e5f9a2db3fc79234d141951e4f17e6a396c
acsnorbika <acsnorbert@turr.hu>
2025-04-09 11:30:42 +0200

    Hibátlanul működik

---
+++ CasinoGep/DatabaseHelper.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SQLite;
+
+namespace CasinoGep
+{
+    public class DatabaseHelper
+    {
+        private string dbFile;
+
+        public DatabaseHelper(string dbFilename)
+        {
+            dbFile = dbFilename;
+
+            // Adatbázis létrehozása, ha nem létezik
+            if (!File.Exists(dbFile))
+            {
+                SQLiteConnection.CreateFile(dbFile);
+            }
+
+            InitializeDatabase();
+        }
+
+        // Adatbázis inicializálása
+        public void InitializeDatabase()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                // Játékosok tábla létrehozása
+                string createPlayersTable = @"
+                CREATE TABLE IF NOT EXISTS Players (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL,
+                    Balance INTEGER NOT NULL
+                )";
+
+                // Játékok tábla létrehozása
+                string createGamesTable = @"
+                CREATE TABLE IF NOT EXISTS Games (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL,
+                    Bet INTEGER NOT NULL
+                )";
+
+                using (SQLiteCommand command = new SQLiteCommand(createPlayersTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                using (SQLiteCommand command = new SQLiteCommand(createGamesTable, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+                // Alapértelmezett adatok beszúrása, ha a táblák üresek
+                InsertDefaultData(connection);
+            }
+        }
+
+        // Alapértelmezett adatok beszúrása
+        private void InsertDefaultData(SQLiteConnection connection)
+        {
+            // Ellenőrizzük, hogy vannak-e már adatok
+            string countPlayers = "SELECT COUNT(*) FROM Players";
+            string countGames = "SELECT COUNT(*) FROM Games";
+
+            int playerCount = 0;
+            int gameCount = 0;
+
+            using (SQLiteCommand command = new SQLiteCommand(countPlayers, connection))
+            {
+                playerCount = Convert.ToInt32(command.ExecuteScalar());
+            }
+
+            using (SQLiteCommand command = new SQLiteCommand(countGames, connection))
+            {
+                gameCount = Convert.ToInt32(command.ExecuteScalar());
+            }
+
+            // Ha nincsenek játékosok, hozzáadunk néhányat
+            if (playerCount == 0)
+            {
+                string insertPlayersQuery = @"
+                INSERT INTO Players (Name, Balance) VALUES
+                ('Játékos 1', 1000),
+                ('Játékos 2', 500),
+                ('Játékos 3', 2000)";
+
+                using (SQLiteCommand command = new SQLiteCommand(insertPlayersQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            // Ha nincsenek játékok, hozzáadunk néhányat
+            if (gameCount == 0)
+            {
+                string insertGamesQuery = @"
+                INSERT INTO Games (Name, Bet) VALUES
+                ('Slot Machine', 50),
+                ('Blackjack', 100),
+                ('Rulett', 75)";
+
+                using (SQLiteCommand command = new SQLiteCommand(insertGamesQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Játékosok lekérdezése
+        public List<Player> GetAllPlayers()
+        {
+            List<Player> players = new List<Player>();
+
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Players";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            players.Add(new Player
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Name = reader["Name"].ToString(),
+                                Balance = Convert.ToInt32(reader["Balance"])
+                            });
+                        }
+                    }
+                }
+            }
+
+            return players;
+        }
+
+        // Játékok lekérdezése
+        public List<Game> GetAllGames()
+        {
+            List<Game> games = new List<Game>();
+
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Games";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            games.Add(new Game
+                            {
+                                Id = Convert.ToInt32(reader["Id"]),
+                                Name = reader["Name"].ToString(),
+                                Bet = Convert.ToInt32(reader["Bet"])
+                            });
+                        }
+                    }
+                }
+            }
+
+            return games;
+        }
+
+        // Új játékos létrehozása
+        public void InsertPlayer(Player player)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Players (Name, Balance) VALUES (@Name, @Balance)";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", player.Name);
+                    command.Parameters.AddWithValue("@Balance", player.Balance);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Játékos törlése
+        public void DeletePlayer(int id)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM Players WHERE Id = @Id";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Játékos frissítése
+        public void UpdatePlayer(Player player)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "UPDATE Players SET Name = @Name, Balance = @Balance WHERE Id = @Id";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", player.Id);
+                    command.Parameters.AddWithValue("@Name", player.Name);
+                    command.Parameters.AddWithValue("@Balance", player.Balance);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Új játék létrehozása
+        public void InsertGame(Game game)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Games (Name, Bet) VALUES (@Name, @Bet)";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", game.Name);
+                    command.Parameters.AddWithValue("@Bet", game.Bet);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Játék törlése
+        public void DeleteGame(int id)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "DELETE FROM Games WHERE Id = @Id";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // Játék frissítése
+        public void UpdateGame(Game game)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection($"Data Source={dbFile};Version=3;"))
+            {
+                connection.Open();
+
+                string query = "UPDATE Games SET Name = @Name, Bet = @Bet WHERE Id = @Id";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", game.Id);
+                    command.Parameters.AddWithValue("@Name", game.Name);
+                    command.Parameters.AddWithValue("@Bet", game.Bet);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+}
--- CasinoGep/MainWindow.xaml.cs
+++ CasinoGep/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace CasinoGep
 {
@@ -16,51 +20,182 @@ namespace CasinoGep
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DatabaseHelper dbHelper;
+        private ObservableCollection<Player> players;
+        private ObservableCollection<Game> games;
+        private Random random = new Random();
+        private Player currentPlayer;
         public MainWindow()
         {
             InitializeComponent();
+
+            dbHelper = new DatabaseHelper("casino.db");
+
+
+            LoadData();
+
+            currentPlayer = new Player { Id = 1, Name = "Játékos", Balance = 1000 };
+            UpdateBalanceDisplay();
+        }
+
+        private void UpdateBalanceDisplay()
+        {
+            BalanceText.Text = $"Egyenleg: {currentPlayer.Balance}";
+        }
+
+        private void LoadData()
+        {
+            players = new ObservableCollection<Player>(dbHelper.GetAllPlayers());
+            PlayersGrid.ItemsSource = players;
+
+            games = new ObservableCollection<Game>(dbHelper.GetAllGames());
+            GamesGrid.ItemsSource = games;
         }
 
         private void PlayersButton_Click(object sender, RoutedEventArgs e)
         {
-
+            PlayersView.Visibility = Visibility.Visible;
+            GamesView.Visibility = Visibility.Collapsed;
+            SlotView.Visibility = Visibility.Collapsed;
         }
 
         private void GamesButton_Click(object sender, RoutedEventArgs e)
         {
-
+            PlayersView.Visibility = Visibility.Collapsed;
+            GamesView.Visibility = Visibility.Visible;
+            SlotView.Visibility = Visibility.Collapsed;
         }
         private void SlotButton_Click(object sender, RoutedEventArgs e)
         {
-
+            PlayersView.Visibility = Visibility.Collapsed;
+            GamesView.Visibility = Visibility.Collapsed;
+            SlotView.Visibility = Visibility.Visible;
         }
         private void AddPlayer_Click(object sender, RoutedEventArgs e)
         {
+            Player newPlayer = new Player
+            {
+                Name = "Új Játékos",
+                Balance = 500
+            };
 
+            dbHelper.InsertPlayer(newPlayer);
+            LoadData();
         }
+    }
         private void DeletePlayer_Click(object sender, RoutedEventArgs e)
         {
-
+        Player selectedPlayer = PlayersGrid.SelectedItem as Player;
+        if (selectedPlayer != null)
+        {
+            dbHelper.DeletePlayer(selectedPlayer.Id);
+            LoadData();
         }
+        else
+        {
+            MessageBox.Show("Válassz ki egy játékost!");
+        }
+    }
         private void UpdatePlayer_Click(object sender, RoutedEventArgs e)
         {
-
+        Player selectedPlayer = PlayersGrid.SelectedItem as Player;
+        if (selectedPlayer != null)
+        {
+            // Egyszerű módosítás: növeljük az egyenleget
+            selectedPlayer.Balance += 100;
+            dbHelper.UpdatePlayer(selectedPlayer);
+            LoadData();
         }
+        else
+        {
+            MessageBox.Show("Válassz ki egy játékost!");
+        }
+    }
         private void Spin_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Tudod mit porgess");
+        int bet = 50;
+
+        if (currentPlayer.Balance < bet)
+        {
+            MessageBox.Show("Nincs elég egyenleged!");
+            return;
         }
+
+        currentPlayer.Balance -= bet;
+
+        // Random számok generálása
+        int num1 = random.Next(1, 8);
+        int num2 = random.Next(1, 8);
+        int num3 = random.Next(1, 8);
+
+        // Számok megjelenítése
+        Slot1.Text = num1.ToString();
+        Slot2.Text = num2.ToString();
+        Slot3.Text = num3.ToString();
+
+        // Nyeremény kiszámítása
+        int win = 0;
+        string resultMessage = "";
+
+        if (num1 == num2 && num2 == num3)
+        {
+            // Három egyforma szám
+            win = bet * 10;
+            resultMessage = "JACKPOT! Nyereményed: " + win;
+        }
+        else if (num1 == num2 || num2 == num3 || num1 == num3)
+        {
+            // Két egyforma szám
+            win = bet * 2;
+            resultMessage = "Nyertél! Nyereményed: " + win;
+        }
+        else
+        {
+            resultMessage = "Sajnos most nem nyertél.";
+        }
+
+        // Nyeremény hozzáadása az egyenleghez
+        currentPlayer.Balance += win;
+
+        // UI frissítése
+        ResultText.Text = resultMessage;
+        UpdateBalanceDisplay();
+    }
         private void AddGame_Click(object sender, RoutedEventArgs e)
         {
+        Game newGame = new Game
+        {
+            Name = "Új Játék",
+            Bet = 50
+        };
 
-        }
+        dbHelper.InsertGame(newGame);
+        LoadData();
+    }
         private void DeleteGame_Click(object sender, RoutedEventArgs e)
         {
+        Game newGame = new Game
+        {
+            Name = "Új Játék",
+            Bet = 50
+        };
 
-        }
+        dbHelper.InsertGame(newGame);
+        LoadData();
+    }
         private void UpdateGame_Click(object sender, RoutedEventArgs e)
         {
-
+        Game selectedGame = GamesGrid.SelectedItem as Game;
+        if (selectedGame != null)
+        {
+            selectedGame.Bet += 10;
+            dbHelper.UpdateGame(selectedGame);
+            LoadData();
         }
+        else
+        {
+            MessageBox.Show("Válassz ki egy játékot!");
+        }
+    }
     }
 }
